3472e605d3ebe958ff75f30f27628bf7eb7fa897
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-29 17:11:50 +0300

    COMPLETED: Validation Messages and Error Handling

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  // error handling
+  const [errors, setErrors] = useState([]);
+
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -14,17 +17,40 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   fetch("/movies", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(formData),
+  //   }).then((res) => {
+  //     if (res.ok) {
+  //       res.json().then((newMovie) => console.log(newMovie));
+  //     } else {
+  //       res.json().then((errorData) => setErrors(errorData.errors));
+  //     }
+  //   });
+  // }
+
+  // using async await
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    // fetch returns a Promise, we must await it
+    const response = await fetch("/movies", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+
+    // response.json() returns a Promise, we must await it
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
 
   function handleChange(e) {
@@ -125,6 +151,15 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+
+        {/* rendering error on-screen */}
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
